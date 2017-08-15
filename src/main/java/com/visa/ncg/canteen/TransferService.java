dd2bbc3f5937254807394fbb5cc1a5e1a3d50b6c
Eric Zhang <erzhang@visa.com>
2017-08-15 13:35:05 -0600

    a

--- src/main/java/com/visa/ncg/canteen/TransferService.java
+++ src/main/java/com/visa/ncg/canteen/TransferService.java
@@ -2,8 +2,15 @@ package com.visa.ncg.canteen;
 
 public class TransferService {
 
-  public void transfer(ExternalAccount externalAccount, Account account, int amount) {
-    externalAccount.withdraw(amount);
-    account.deposit(amount);
+  public void transfer(ExternalAccount externalAccount, Account account, int amount) throws IllegalTransferAmountException, InsufficientFundsException {
+    if (amount <= 0) {
+      throw new IllegalTransferAmountException();
+    } else if (externalAccount.getBalance() < amount) {
+      throw new InsufficientFundsException();
+    } else {
+      externalAccount.withdraw(amount);
+      account.deposit(amount);
+    }
+
   }
 }
--- src/test/java/com/visa/ncg/canteen/TransferServiceTest.java
+++ src/test/java/com/visa/ncg/canteen/TransferServiceTest.java
@@ -3,12 +3,13 @@ package com.visa.ncg.canteen;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class TransferServiceTest {
 
   @Test
   public void transferTenDollarsFromExternalAccountToAccountIncreasesAccountBalance() throws Exception {
-    ExternalAccount externalAccount = new ExternalAccount();
+    ExternalAccount externalAccount = new ExternalAccount(10);
     Account account = new Account();
     TransferService transferService = new TransferService();
 
@@ -18,4 +19,22 @@ public class TransferServiceTest {
         .isEqualTo(10);
   }
 
+  @Test
+  public void insufficientFundsTest() throws Exception {
+    ExternalAccount externalAccount = new ExternalAccount(0);
+    Account account = new Account();
+    TransferService transferService = new TransferService();
+
+    assertThatExceptionOfType(InsufficientFundsException.class).isThrownBy(() -> { transferService.transfer(externalAccount, account, 10);});
+
+  }
+
+  @Test
+  public void illegalTransferAmountTest() throws Exception {
+    ExternalAccount externalAccount = new ExternalAccount(10);
+    Account account = new Account();
+    TransferService transferService = new TransferService();
+
+    assertThatExceptionOfType(IllegalTransferAmountException.class).isThrownBy(() -> {transferService.transfer(externalAccount, account, 0);});
+  }
 }
